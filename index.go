33719780c2553b27808050293de09528d5d5485b
Mathieu Pavageau <mth.pvg@gmail.com>
2018-01-27 08:00:41 +0100

    chore: one function multiple returns

--- index.go
+++ index.go
@@ -13,6 +13,10 @@ func add(x, y int) int {
 func addMixed(x float32, y int) int {
 	return int(x) + y
 }
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -MAIN
 func main() {
   fmt.Println("Hello World, 世界")
@@ -20,5 +24,7 @@ func main() {
   fmt.Printf("Math, exported name Pi: %g.\n", math.Pi)
   fmt.Printf("Function, result: %d.\n", add(4, 2))
   fmt.Printf("Function, result: %d.\n", addMixed(1.4142, 2))
+  a, b := swap("results", "Multiple")
+  fmt.Println(a, b)
 }
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - END
