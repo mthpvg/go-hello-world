50b6ad49bcdbc99659df51483d162c6fa4b1c1b6
Mathieu Pavageau <mth.pvg@gmail.com>
2018-01-27 07:24:30 +0100

    chore: play with import math

--- index.go
+++ index.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "math"
+)
 
 func main() {
-	fmt.Println("Hello, 世界")
+  fmt.Println("Hello World, 世界")
+  fmt.Printf("Math, square root of 7 is: %g.\n", math.Sqrt(7))
+  fmt.Printf("Math, exported name Pi: %g.\n", math.Pi)
 }
