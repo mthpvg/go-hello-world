d134ae00d3fbd24582cf263a278f5d0d7c7f3129
Mathieu Pavageau <mth.pvg@gmail.com>
2018-01-27 07:52:36 +0100

    chore: functions

--- index.go
+++ index.go
@@ -6,13 +6,19 @@ import (
   "math"
 )
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - FUNCTIONS
-func add(x int, y int) int {
+func add(x, y int) int {
 	return x + y
+}
+
+func addMixed(x float32, y int) int {
+	return int(x) + y
 }
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -MAIN
 func main() {
   fmt.Println("Hello World, 世界")
   fmt.Printf("Math, square root of 7 is: %g.\n", math.Sqrt(7))
   fmt.Printf("Math, exported name Pi: %g.\n", math.Pi)
+  fmt.Printf("Function, result: %d.\n", add(4, 2))
+  fmt.Printf("Function, result: %d.\n", addMixed(1.4142, 2))
 }
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - END
