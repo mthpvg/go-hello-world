824772610129edb136ee1d339a34fa2346f65d5f
Mathieu Pavageau <mth.pvg@gmail.com>
2018-01-27 07:27:52 +0100

    chore: format

--- index.go
+++ index.go
@@ -1,12 +1,18 @@
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - BEGIN
 package main
-
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -IMPORT
 import (
   "fmt"
   "math"
 )
-
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - FUNCTIONS
+func add(x int, y int) int {
+	return x + y
+}
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -MAIN
 func main() {
   fmt.Println("Hello World, 世界")
   fmt.Printf("Math, square root of 7 is: %g.\n", math.Sqrt(7))
   fmt.Printf("Math, exported name Pi: %g.\n", math.Pi)
 }
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - END
